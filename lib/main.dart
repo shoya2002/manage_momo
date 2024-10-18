fd24dd085a62f3992a0a4854d867fde23861576a
Shoya Furukawa <sosyoccer.2559@icloud.com>
2024-10-18 13:49:49 +0900

    shoya

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
+import 'setting_screen.dart';
+import 'friend_screen.dart';
+import 'login.dart';
+import 'register.dart';
+import 'task_manager.dart';
 import 'package:flutter/material.dart';
+import 'welcome_screen.dart';
+import 'complete_task.dart';
+import 'event_screen.dart';
 
-void main() => runApp(BMICalculator());
+void main() {
+  runApp(Flash_chat());
+}
 
-class BMICalculator extends StatelessWidget {
+class Flash_chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegisterScreen.id: (context) => RegisterScreen(),
+        TaskManager.id: (context) => TaskManager(),
+        SettingsScreen.id: (context) => SettingsScreen(),
+        FriendScreen.id: (context) => FriendScreen(),
+        CompletedTasksScreen.id: (context) => CompletedTasksScreen(),
+        EventScreen.id: (context) => EventScreen()
+      },
     );
   }
 }
